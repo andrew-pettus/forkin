3b9126817f3a35101df11ca8df9364219c99521a
pettu <the.real.andrew.pettus@gmail.com>
2022-10-12 18:21:49 -0500

    Added client, chose app so removed scope,

--- flask_docker/app/routes.py
+++ flask_docker/app/routes.py
@@ -1,13 +1,12 @@
 from flask import render_template, request
 from app import app
 
-GITHUB_CLIENT = '1234'
-SCOPES = '??'
+GITHUB_CLIENT = 'Iv1.21156d618ca695a2'
 
 OWNER = 'andrew-pettus'
 REPO  = 'forkin'
 
 @app.route('/')
 def home():
-    return render_template("index.html", clientID = GITHUB_CLIENT, scopes = SCOPES)
+    return render_template("index.html", clientID = GITHUB_CLIENT)
 
